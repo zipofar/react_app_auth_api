e287ace8a397956512231ee113ff4eaa5826f3fc
ingprog <zipofar@gmail.com>
2018-06-29 13:19:51 +0300

    add function validation date

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -10,6 +10,20 @@ export const validateFormProfile = (values) => {
         errors.email = 'Required'
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address'
+    } else if (isInvalidDate(values.birthday)) {
+        errors.birthday = 'Invalid date of birthday';
     }
     return errors;
+};
+
+const isInvalidDate = (date) => {
+    if (isNaN(new Date(date).getDate())) {
+        return true;
+    }
+
+    if (new Date(date) > Date.now()) {
+        return true;
+    }
+
+    return false;
 };
